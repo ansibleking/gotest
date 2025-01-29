599380fc93be68c978c632868de7301f1d02093c
ansibleking <130787587+ansibleking@users.noreply.github.com>
2025-01-30 00:48:29 +0400

    Create helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "{\"message\": \"Hello, World!\"}")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+
+	port := "8080"
+	log.Printf("Server is running on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
